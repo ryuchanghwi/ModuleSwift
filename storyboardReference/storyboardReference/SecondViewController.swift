c03b77546cdfb4caea662797676eb057d9ab9062
ryuchanghwi <ckdgnl10@naver.com>
2021-12-21 00:12:55 +0900

    storyboardReferencePractice
    스토리보드레퍼런스를 이용하여 네비게이션 텝바 구현

---
+++ storyboardReference/storyboardReference/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  storyboardReference
+//
+//  Created by 류창휘 on 2021/12/03.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
