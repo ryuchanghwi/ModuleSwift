e32e1b50eaf76cc26230b68a179bd7611291ed67
ryuchanghwi <ckdgnl10@naver.com>
2022-01-08 04:40:04 +0900

    update

--- NotificationCenterPractice/NotificationCenterPractice/ThirdViewController.swift
+++ NotificationCenterPractice/NotificationCenterPractice/ThirdViewController.swift
@@ -8,13 +8,30 @@
 import Foundation
 import UIKit
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var ThirdLabel: UILabel!
     @IBOutlet weak var myTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        myTextField.delegate = self
     }
+    
+    //키보드 입력 완료 버튼을 눌렀을때 키보드 사라지게 하기
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //화면 아무곳이나 터치 해도 키보드가 사라지게 하기
+    override func  touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        <#code#>
+    }
+    
     @IBAction func goFirstVCBtn(_ sender: Any) {
         guard let vc = self.storyboard?.instantiateViewController(identifier: "ViewController") else { return }
         vc.modalPresentationStyle = .fullScreen
